7878ca2dd3b9d80297a3b4bf6adb569f6d065014
Ananya Shetty <ananyashetty069@gmail.com>
2025-09-05 13:45:05 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.1.0",
+    author="Ananya Shetty",
+    author_email="ananyashetty069@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
